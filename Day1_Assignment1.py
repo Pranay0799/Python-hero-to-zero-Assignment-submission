aa1331b0723aa9fabd8917a162dc1d7971f93a26
Pranay0799 <90129528+Pranay0799@users.noreply.github.com>
2021-09-05 13:23:53 +0530

    Day 1 Assignment Submitted

---
+++ Day1_Assignment1.py
@@ -0,0 +1,58 @@
+#COURSE - Python ZERO TO HERO 
+# Questons 1,2,3,4//
+
+
+### QUESTION 1) Add two number
+num1 = 123
+num2 = 456
+Add = num1 + num2
+print("Addition of two number:", Add)
+
+### OUTPUT Q1 = 579
+
+
+
+
+### QUESTION 2) Try to add integer with string and see the output
+'''
+n1 = 123
+s1 = "Hellow"
+add_n1_s1 = n1 + s1
+print("Add integer with string output is :",add_n1_s1)
+'''
+
+### OUTPUT Q2 = TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+
+
+
+
+### QUESTION 3) Print varible and string in one line with the help of f string
+name_of_country = "INDIA"
+print(f"I Am from {name_of_country}")
+
+### OUTPUT Q3 =I Am from INDIA
+
+
+
+
+
+### QUESTION 4) How to add space and new line in in the print statement
+
+a1 = "Maharashtra"
+a2 = "India"
+print(a1,"\t",a2)  # add space ==> ##Output==> Maharashtra      India
+
+
+print(a1,"\n",a2)  # add new line  ##Output=> Maharashtra    
+                                    #         India
+
+
+print(a1,"\n\t",a2)  # add space and newline
+
+### OUTPUT Q4 : Maharashtra
+#                     India
+
+
+
+
