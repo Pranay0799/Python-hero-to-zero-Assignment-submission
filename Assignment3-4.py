8a2dbadca5aaaae250e8187f70dcecaff48e19ab
Pranay0799 <90129528+Pranay0799@users.noreply.github.com>
2021-09-07 20:31:03 +0530

    Day3 assignment3 and 4 is submitted

---
+++ Assignment3-4.py
@@ -0,0 +1,45 @@
+###PYTHON HERO TO ZERO
+### ASSIGNMENT NO 3
+
+
+def fun1(a, b= 0, *args, **kwargs):
+    print(a, b, args, kwargs)
+
+fun1(12, 23, 45, 65, 78, 98, 41, 35, 86, 97, 79, city = "Mumbai", State = "Maharashtra", Country = "India")
+
+
+
+
+
+
+
+### ASSIGNMENT NO 4 
+
+# Question : Write a program to implement insertion sort
+
+def insertion_Sort(lis):
+    for i in range(1, len(lis)):
+        tem = lis[i]
+        j = i - 1
+        while (j >= 0 and tem < lis[j]):
+            lis[j + 1] = lis[j]
+            j = j - 1
+        lis[j + 1] = tem
+
+lis = input("Enter the list of no. : ").split()
+lis = [int(x) for x in lis]
+insertion_Sort(lis)
+print("Sorted list : ", end=" ")
+print(lis)
+
+
+
+### Output
+'''
+input : 12 32 45 85 98 75 105 450 9 73 99
+Output : ===> Sorted list :  [9, 12, 32, 45, 73, 75, 85, 98, 99, 105, 450]
+'''
+
+
+
+
