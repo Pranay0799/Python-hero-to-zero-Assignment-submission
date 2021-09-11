f307e7da0c020949e1cfeee0771289402faa9c1b
Pranay0799 <90129528+Pranay0799@users.noreply.github.com>
2021-09-11 13:48:25 +0530

    Data Science Assignment No 2 is submitted

---
+++ DataScience_Assignment_No_2.py
@@ -0,0 +1,17 @@
+# Course : Data Science
+
+#Assignment No 2
+
+####Problem : From the above / Below array, retrieve the values([[3,4],[5,6]]) using indexing
+import numpy as np
+
+
+arr1 = np.array([[1, 2, 3, 4, 5],
+                 [2, 3, 4, 5, 6],
+                 [4, 5, 6, 7, 8]])
+
+
+print(arr1[1:,1:3])
+
+
+#
